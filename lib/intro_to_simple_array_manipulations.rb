9cd1f3d4d1a53cb834a8a79b448a61ea68b26f1f
Adri Torres <adriana.amaral@msn.com>
2020-10-27 19:17:10 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,28 @@
+def using_push (array, string)
+  array.push(string)
+end
+
+def using_unshift (array, string)
+  array.unshift(string)
+end
+
+def using_pop(continents)
+  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  array.pop
+end
+
+def pop_with_args(dog_breeds)
+  dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+  array.pop(2)
+end
+
+def using_shift(array)
+  array = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+array.shift
+end
+
+def shift_with_args(array)
+  array = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+ array.shift(2)
+end
+
